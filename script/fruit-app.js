da80fbdfb3b7b155deb956a952ec574055bd4ec7
Juuso Rautiainen <juuso.rautiainen@gmail.com>
2021-10-05 23:42:41 +0300

    -

--- script/fruit-app.js
+++ script/fruit-app.js
@@ -1,29 +1,41 @@
+// Read existing items from LocalStorage
 const itemList = getSavedItems();
+// Generate the DOM structure for a items
 renderItems(itemList);
 
+// Listener for adding new item to list
 document.querySelector("#new-fruit").addEventListener("submit", function (ev) {
   ev.preventDefault();
+  // Push item to list
   itemList.push({
     ingredient: ev.target.elements.fruit.value,
     price: ev.target.elements.price.value,
   });
+  // Display Highest and Lowest
+  lowhigh();
+  // Push item to localStorage function
   pushLocal(itemList);
+  // Render new item to DOM
   renderItems(itemList);
+  // Clear form input fields
   ev.target.elements.fruit.value = "";
   ev.target.elements.price.value = "";
 });
 
+// Listener for Clear all, both localStorage & DOM
 document.querySelector("#clear-list").addEventListener("click", function (ev) {
   ev.preventDefault();
   clearItems();
 });
 
-document.querySelector('#ascending').addEventListener('click', function (ev){
+// Listener for Sort by ascending order
+document.querySelector("#ascending").addEventListener("click", function (ev) {
   ev.preventDefault();
   ascending();
-})
+});
 
-document.querySelector('#descending').addEventListener('click', function (ev){
+// Listener for Sort by descending order
+document.querySelector("#descending").addEventListener("click", function (ev) {
   ev.preventDefault();
   descending();
-})
+});
--- script/fruit-functions.js
+++ script/fruit-functions.js
@@ -2,6 +2,7 @@
 const getSavedItems = function () {
   const itemsJSON = localStorage.getItem("itemList");
 
+  // Read localStorage if !== null, return empty Array
   if (itemsJSON !== null) {
     return JSON.parse(itemsJSON);
   } else {
@@ -13,27 +14,29 @@ const getSavedItems = function () {
 const renderItems = function (itemList) {
   document.querySelector("#fruits").innerHTML = "";
 
+  // Summary to show amount of items
   const summary = document.createElement("p");
   summary.textContent = `Amount of items on your list ${itemList.length}.`;
   document.querySelector("#fruits").appendChild(summary);
 
+  // Loop through Array to display items
   itemList.forEach(function (item) {
-    const p = document.createElement("ul");
-    let price = parseFloat(item.price)
-    p.textContent = item.ingredient + " " + price.toFixed(2) + " €";
-    document.querySelector("#fruits").appendChild(p);
+    const ul = document.createElement("ul");
+    let price = parseFloat(item.price);
+    ul.textContent = item.ingredient + " " + price.toFixed(2) + " €";
+    document.querySelector("#fruits").appendChild(ul);
+    lowhigh();
   });
 };
 
 // Push item to LocalStorage
 const pushLocal = function (itemList) {
   localStorage.setItem("itemList", JSON.stringify(itemList));
-  
 };
 
 // Clear DOM and localStorage
 const clearItems = function () {
-  const items = document.querySelectorAll("fruits");
+  const items = document.querySelectorAll("fruits", "low");
   items.forEach(function (p) {
     p.remove();
   });
@@ -42,6 +45,9 @@ const clearItems = function () {
   }
   localStorage.clear();
   renderItems(itemList);
+  // Clear high and low innerHTML
+  (document.getElementById("high").innerHTML = ""),
+    (document.getElementById("low").innerHTML = "");
 };
 
 // Sort Low to High
@@ -66,4 +72,35 @@ const descending = function () {
     p.textContent = item.ingredient + " " + item.price + " €";
   });
   renderItems(itemList);
+};
+
+// Display Most affordable and most Expensive, innerHTML
+const lowhigh = function () {
+  
+    let highestPrice = itemList.sort(
+      (a, b) => parseFloat(b.price) - parseFloat(a.price)
+    );
+    localStorage.setItem("high", JSON.stringify(highestPrice));
+    let dfPrice = parseFloat(highestPrice[0].price);
+    document.getElementById("high").innerHTML =
+      "Most expensive item on your list: " +
+      "&nbsp;" +
+      highestPrice[0].ingredient +
+      " " +
+      dfPrice.toFixed(2) +
+      " €";
+
+    let lowestPrice = itemList.sort(
+      (a, b) => parseFloat(a.price) - parseFloat(b.price)
+    );
+    localStorage.setItem("low", JSON.stringify(lowestPrice));
+    let dfPriceb = parseFloat(lowestPrice[0].price);
+    document.getElementById("low").innerHTML =
+      "Most affordable item on your list: " +
+      "&nbsp;" +
+      lowestPrice[0].ingredient +
+      " " +
+      dfPriceb.toFixed(2) +
+      " €";
+  
 };
